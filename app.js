fe649d943bf467f55c8d4df0d9ac50b9c2d9ac9b
Sandeep <sandeepeos04@gmail.com>
2023-04-11 14:41:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to NHK");
+    return;
+  }
+  if (req.url === "/about") {
+    res.end("About Page");
+    return;
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.end(`
+    <h2>Error</h2>
+    <h4>Page not found!</h4>
+    <a href = '/'>Back Home</a>`);
+  return;
+});
+server.listen(5000);
+
+// const amount = 8;
+// let i = 0;
+// let lyrics = ["Bikramm", "Naayagan", "Meendum", "Varraar"];
+// if (amount < 10) {
+//   console.log("Small Number");
+// } else {
+//   console.log("Large Number");
+// }
+// const bikram = (lyrics) => {
+//   if (i > lyrics.length) {
+//     i = 0;
+//   }
+//   console.log(lyrics[i]);
+//   i++;
+// };
+// setInterval(() => {
+//   console.log("Vikramm");
+//   console.log("Bikramm");
+// }, 600);
+
+// const os = require("os");
+// const user = os.totalmem();
+// console.log(user);
+// const user1 = os.freemem();
+// console.log(user1);
+// const uptime = os.uptime();
+// console.log(uptime);
+
+// const path = require("path");
+// console.log(path.join("/content/", "subfolder", "test.txt"));
+
+// const { log } = require("console");
+
+//fs
+// const fs = require("fs");
+
+//fs-sync
+
+// const first = fs.readFileSync("./content/first.txt", "utf8");
+// const second = fs.readFileSync("./content/second.txt", "utf8");
+// fs.writeFileSync(
+//   "./content/result.txt",
+//   `Here is the Content\n${first}\n${second}`
+// );
+
+//fs-async
+
+// fs.readFile("./content/first.txt", "utf8", (err, result) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   const firstS = result;
+//   fs.readFile("./content/second.txt", "utf8", (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     const secondS = result;
+//     fs.writeFile(
+//       "./content/result-async.txt",
+//       `This was written by Async\n${firstS}\n${secondS}`,
+//       (err, result) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log(result);
+//       }
+//     );
+//   });
+// });
